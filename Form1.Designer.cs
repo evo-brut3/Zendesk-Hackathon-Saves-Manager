47cbfb6b39b6cf864bf26a42bdf991ab7bc33f3a
Hajaro <m.remiszewicz@gmail.com>
2019-10-26 15:12:34 +0200

    Improve the design of the GUI profiles and games' lists

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,48 +30,35 @@
         {
             this.deleteprofile = new System.Windows.Forms.Button();
             this.deletegame = new System.Windows.Forms.Button();
-            this.profileListView = new System.Windows.Forms.ListView();
             this.addgame = new System.Windows.Forms.Button();
             this.addprofile = new System.Windows.Forms.Button();
-            this.gameListView = new System.Windows.Forms.ListView();
+            this.gameListView = new System.Windows.Forms.ListBox();
+            this.profileListView = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // deleteprofile
             // 
-            this.deleteprofile.Location = new System.Drawing.Point(640, 495);
-            this.deleteprofile.Margin = new System.Windows.Forms.Padding(4);
+            this.deleteprofile.Location = new System.Drawing.Point(480, 402);
             this.deleteprofile.Name = "deleteprofile";
-            this.deleteprofile.Size = new System.Drawing.Size(133, 68);
+            this.deleteprofile.Size = new System.Drawing.Size(100, 55);
             this.deleteprofile.TabIndex = 14;
             this.deleteprofile.Text = "Delete Profile";
             this.deleteprofile.UseVisualStyleBackColor = true;
             // 
             // deletegame
             // 
-            this.deletegame.Location = new System.Drawing.Point(216, 495);
-            this.deletegame.Margin = new System.Windows.Forms.Padding(4);
+            this.deletegame.Location = new System.Drawing.Point(162, 402);
             this.deletegame.Name = "deletegame";
-            this.deletegame.Size = new System.Drawing.Size(133, 68);
+            this.deletegame.Size = new System.Drawing.Size(100, 55);
             this.deletegame.TabIndex = 13;
             this.deletegame.Text = "Delete Game";
             this.deletegame.UseVisualStyleBackColor = true;
             // 
-            // profileListView
-            // 
-            this.profileListView.HideSelection = false;
-            this.profileListView.Location = new System.Drawing.Point(440, 15);
-            this.profileListView.Margin = new System.Windows.Forms.Padding(4);
-            this.profileListView.Name = "profileListView";
-            this.profileListView.Size = new System.Drawing.Size(332, 457);
-            this.profileListView.TabIndex = 10;
-            this.profileListView.UseCompatibleStateImageBehavior = false;
-            // 
             // addgame
             // 
-            this.addgame.Location = new System.Drawing.Point(16, 495);
-            this.addgame.Margin = new System.Windows.Forms.Padding(4);
+            this.addgame.Location = new System.Drawing.Point(12, 402);
             this.addgame.Name = "addgame";
-            this.addgame.Size = new System.Drawing.Size(133, 68);
+            this.addgame.Size = new System.Drawing.Size(100, 55);
             this.addgame.TabIndex = 9;
             this.addgame.Text = "Add Game";
             this.addgame.UseVisualStyleBackColor = true;
@@ -79,38 +66,40 @@
             // 
             // addprofile
             // 
-            this.addprofile.Location = new System.Drawing.Point(440, 495);
-            this.addprofile.Margin = new System.Windows.Forms.Padding(4);
+            this.addprofile.Location = new System.Drawing.Point(330, 402);
             this.addprofile.Name = "addprofile";
-            this.addprofile.Size = new System.Drawing.Size(133, 68);
+            this.addprofile.Size = new System.Drawing.Size(100, 55);
             this.addprofile.TabIndex = 8;
             this.addprofile.Text = "Add Profile";
             this.addprofile.UseVisualStyleBackColor = true;
             // 
             // gameListView
             // 
-            this.gameListView.HideSelection = false;
-            this.gameListView.Location = new System.Drawing.Point(16, 15);
-            this.gameListView.Margin = new System.Windows.Forms.Padding(4);
+            this.gameListView.FormattingEnabled = true;
+            this.gameListView.Location = new System.Drawing.Point(12, 16);
             this.gameListView.Name = "gameListView";
-            this.gameListView.Size = new System.Drawing.Size(332, 457);
-            this.gameListView.TabIndex = 12;
-            this.gameListView.UseCompatibleStateImageBehavior = false;
-            this.gameListView.View = System.Windows.Forms.View.List;
-            this.gameListView.SelectedIndexChanged += new System.EventHandler(this.Profile_SelectedIndexChanged);
+            this.gameListView.Size = new System.Drawing.Size(250, 368);
+            this.gameListView.TabIndex = 15;
+            // 
+            // profileListView
+            // 
+            this.profileListView.FormattingEnabled = true;
+            this.profileListView.Location = new System.Drawing.Point(330, 16);
+            this.profileListView.Name = "profileListView";
+            this.profileListView.Size = new System.Drawing.Size(250, 368);
+            this.profileListView.TabIndex = 16;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(791, 577);
+            this.ClientSize = new System.Drawing.Size(593, 469);
+            this.Controls.Add(this.profileListView);
+            this.Controls.Add(this.gameListView);
             this.Controls.Add(this.deleteprofile);
             this.Controls.Add(this.deletegame);
-            this.Controls.Add(this.profileListView);
             this.Controls.Add(this.addgame);
             this.Controls.Add(this.addprofile);
-            this.Controls.Add(this.gameListView);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -121,10 +110,10 @@
 
         private System.Windows.Forms.Button deleteprofile;
         private System.Windows.Forms.Button deletegame;
-        private System.Windows.Forms.ListView profileListView;
         private System.Windows.Forms.Button addgame;
         private System.Windows.Forms.Button addprofile;
-        private System.Windows.Forms.ListView gameListView;
+        private System.Windows.Forms.ListBox gameListView;
+        private System.Windows.Forms.ListBox profileListView;
     }
 }
 
