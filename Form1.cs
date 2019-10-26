4a4f14a12b2e98ac07ddbc4329a26d50261a8a6c
Hajaro <m.remiszewicz@gmail.com>
2019-10-26 14:48:09 +0200

    Fix the placement of Profile class

--- Form1.cs
+++ Form1.cs
@@ -12,43 +12,7 @@ using System.IO;
 
 namespace Zendesk_Hackathon_Saves_Manager
 {
-    public class Profile
-    {
-        public int game_id;
-        public int profile_id;
-        public string game_name;
-        public string profile_name;
-        public string profile_location;
 
-        public Profile(int gid, int pid, string gn, string pn, string pl)
-        {
-            game_id = gid;
-            profile_id = pid;
-            game_name = gn;
-            profile_name = pn;
-            profile_location = pl;
-        }
-    }
-
-    static class ProfileManager
-    {
-        public static List<Profile> ProfileList= new List<Profile>();
-
-        public static List<Profile> GetProfileList()
-        {
-            return ProfileList;
-        }
-
-        public static void AddToProfileList(Profile profile)
-        {
-            ProfileList.Add(profile);
-        }
-
-        public static void RemoveFromProfileList()
-        {
-
-        }
-    }
 
     public partial class Form1 : Form
     {
@@ -96,6 +60,44 @@ namespace Zendesk_Hackathon_Saves_Manager
         private void Addgame_Click(object sender, EventArgs e)
         {
 
+
+        }
+        public class Profile
+        {
+            public int game_id;
+            public int profile_id;
+            public string game_name;
+            public string profile_name;
+            public string profile_location;
+
+            public Profile(int gid, int pid, string gn, string pn, string pl)
+            {
+                game_id = gid;
+                profile_id = pid;
+                game_name = gn;
+                profile_name = pn;
+                profile_location = pl;
+            }
+        }
+
+        static class ProfileManager
+        {
+            public static List<Profile> ProfileList = new List<Profile>();
+
+            public static List<Profile> GetProfileList()
+            {
+                return ProfileList;
+            }
+
+            public static void AddToProfileList(Profile profile)
+            {
+                ProfileList.Add(profile);
+            }
+
+            public static void RemoveFromProfileList()
+            {
+
+            }
         }
     }
 }
